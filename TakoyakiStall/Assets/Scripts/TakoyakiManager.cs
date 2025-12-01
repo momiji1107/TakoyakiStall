50ff4ba307360a99503738c98b2961b8a7cfd15f
momiji1107 <yokohirahazuki0607@gmail.com>
2025-12-01 22:31:49 +0900

    たこ焼きの画像のクリックを検知するようにした。

---
+++ TakoyakiStall/Assets/Scripts/TakoyakiManager.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+
+public class TakoyakiManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        Debug.Log("OnClick");
+    }
+}
