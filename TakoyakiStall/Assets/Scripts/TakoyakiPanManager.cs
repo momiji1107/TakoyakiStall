5f31058c03d9c4730c71dd79f891aff34eae970a
momiji1107 <yokohirahazuki0607@gmail.com>
2025-12-11 23:09:19 +0900

    クリック時のカーソルの座標を取得する
    鉄板の画像をクリックしたときのカーソルの座標を取得するようにした。
    左上を(0,0)としたときのスクリーン座標。
    InputをInputManagerとInputSystemの両方で行う設定に変更した。

---
+++ TakoyakiStall/Assets/Scripts/TakoyakiPanManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class TakoyakiPanManager : MonoBehaviour
+{
+    public void OnClick()
+    {
+        Vector3 cursorPos = Input.mousePosition;
+        Debug.Log(cursorPos);
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
