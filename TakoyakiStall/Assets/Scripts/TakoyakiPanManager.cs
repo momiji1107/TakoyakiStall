0494070862f238ad4e44995f08534d461303c6a8
momiji1107 <yokohirahazuki0607@gmail.com>
2025-12-12 23:48:44 +0900

    鉄板の穴のそれぞれの中心座標を配列に格納した
    holeWidth：穴と穴の距離
    holeStartPos：左上の穴の座標
    panWidth：鉄板の横の穴の数
    panHeight：鉄板の縦の穴の数

--- TakoyakiStall/Assets/Scripts/TakoyakiPanManager.cs
+++ TakoyakiStall/Assets/Scripts/TakoyakiPanManager.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 public class TakoyakiPanManager : MonoBehaviour
 {
+    [SerializeField] private float holewidth = 4.7f;
+    [SerializeField] private float holeStartPos = 7.05f;
+    [SerializeField] private int panWidth = 4;
+    [SerializeField] private int panHeight = 4;
+    private Vector3[] holes = new Vector3[16];
     public void OnClick()
     {
         Vector3 cursorPos = Input.mousePosition;
@@ -10,7 +15,24 @@ public class TakoyakiPanManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        int n = 0;
+        float y = holeStartPos;
+        for (int i = 0; i < panHeight; i++)
+        {
+            float x = -holeStartPos;
+            for (int j = 0; j < panWidth; j++)
+            {
+                holes[n] = new Vector3(x, y, 0f);
+                n++;
+                x += holewidth;
+            }
+            y -= holewidth;
+        }
+
+        foreach (var hole in holes)
+        {
+            Debug.Log(hole);
+        }
     }
 
     // Update is called once per frame
