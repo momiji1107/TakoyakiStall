0368379d78b6efba81eb35744e8f1018ef1b96fe
momiji1107 <yokohirahazuki0607@gmail.com>
2025-12-17 23:52:38 +0900

    スクリーン座標をローカル座標に変換し、選択した穴を判別する処理を追加
    スクリーン座標→ワールド座標→ローカル座標に変換。
    鉄板のそれぞれの穴の中心からの距離を計算し、距離が２より小さい穴を選択した穴として登録する。

--- TakoyakiStall/Assets/Scripts/TakoyakiPanManager.cs
+++ TakoyakiStall/Assets/Scripts/TakoyakiPanManager.cs
@@ -6,11 +6,25 @@ public class TakoyakiPanManager : MonoBehaviour
     [SerializeField] private float holeStartPos = 7.05f;
     [SerializeField] private int panWidth = 4;
     [SerializeField] private int panHeight = 4;
-    private Vector3[] holes = new Vector3[16];
+    private Vector2[] holes = new Vector2[16];
+    private float distance = 0;
+    private Vector2 selectHole = new Vector2();
     public void OnClick()
     {
         Vector3 cursorPos = Input.mousePosition;
-        Debug.Log(cursorPos);
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(cursorPos);
+        Vector3 localPos = transform.InverseTransformPoint(worldPos);
+
+        foreach (Vector2 hole in holes)
+        {
+            distance = Vector2.Distance(new Vector2(hole.x, hole.y), localPos);
+            if (distance < 2.0f)
+            {
+                selectHole = hole;
+                break;
+            }
+        }
+        Debug.Log("selectHole is " + selectHole);
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
